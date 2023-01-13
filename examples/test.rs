93719ad81f1623998fe82130f27e8326555ef79b
Nico Burns <nico@nicoburns.com>
2023-01-13 23:13:42 +0000

    Remove unnecessary iced size styles from example

--- examples/test.rs
+++ examples/test.rs
@@ -50,8 +50,6 @@ impl Sandbox for Example {
     fn view(&self) -> Element<Message> {
         const REALLY_LONG_PARAGRAPH : &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
         grid()
-            .width(Length::Fill)
-            .height(Length::Fill)
             .with_columns(vec![fr(1.), flex(2.), fr(1.)])
             .with_rows(vec![auto(), auto(), fr(1.)])
             .with_column_gap(points(20.))
@@ -59,8 +57,6 @@ impl Sandbox for Example {
             .with_child(rect(20.0, BLACK))
             .with_child({
                 grid()
-                    .width(Length::Fill)
-                    .height(Length::Shrink)
                     .with_styled_child(text(format!("Button clicked {} times", self.click_count)).size(32), |style| {
                         style.align_self = Some(AlignSelf::Center);
                         style.justify_self = Some(AlignSelf::Center);
@@ -76,8 +72,6 @@ impl Sandbox for Example {
             .with_child(rect(20.0, COLOR4))
             .with_child({
                 grid()
-                    .width(Length::Fill)
-                    .height(Length::Fill)
                     .with_columns(vec![fr(1.), fr(2.), fr(1.)])
                     .with_rows(vec![fr(1.), percent(0.5), fr(1.)])
                     .with_child(rect(20.0, COLOR7))
