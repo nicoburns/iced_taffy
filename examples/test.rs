490241d8d8d970f658368e0491892b5bda6e8baa
Nico Burns <nico@nicoburns.com>
2023-01-13 21:01:46 +0000

    Move colors module into it's own file

--- examples/test.rs
+++ examples/test.rs
@@ -1,53 +1,18 @@
-use iced::widget::{text, button};
+use iced::widget::{button, text};
 use iced::{Element, Length, Sandbox, Settings};
 use iced_taffy::grid;
 use taffy::prelude::*;
 
-
 mod common {
     pub mod rect;
+    pub mod colors;
 }
 use common::rect::rect;
-
-#[allow(dead_code)]
-mod colors {
-    use iced::Color;
-
-    pub const BLACK: Color = Color::BLACK;
-    pub const RED: Color = Color::from_rgb(255. / 255., 0., 0.);
-    pub const GREEN: Color = Color::from_rgb(0., 255. / 255., 0.);
-    pub const BLUE: Color = Color::from_rgb(0., 0., 255. / 255.);
-    pub const COLOR1: Color = Color::from_rgb(230. / 255., 25. / 255., 75. / 255.);
-    pub const COLOR2: Color = Color::from_rgb(60. / 255., 180. / 255., 75. / 255.);
-    pub const COLOR3: Color = Color::from_rgb(255. / 255., 225. / 255., 25. / 255.);
-    pub const COLOR4: Color = Color::from_rgb(67. / 255., 99. / 255., 216. / 255.);
-    pub const COLOR5: Color = Color::from_rgb(245. / 255., 130. / 255., 49. / 255.);
-    pub const COLOR6: Color = Color::from_rgb(145. / 255., 30. / 255., 180. / 255.);
-    pub const COLOR7: Color = Color::from_rgb(70. / 255., 240. / 255., 240. / 255.);
-    pub const COLOR8: Color = Color::from_rgb(240. / 255., 50. / 255., 230. / 255.);
-    pub const COLOR9: Color = Color::from_rgb(188. / 255., 246. / 255., 12. / 255.);
-    pub const COLOR10: Color = Color::from_rgb(250. / 255., 190. / 255., 190. / 255.);
-    pub const COLOR11: Color = Color::from_rgb(0., 128. / 255., 128. / 255.);
-    pub const COLOR12: Color = Color::from_rgb(230. / 255., 190. / 255., 255. / 255.);
-    pub const COLOR13: Color = Color::from_rgb(154. / 255., 99. / 255., 36. / 255.);
-    pub const COLOR14: Color = Color::from_rgb(255. / 255., 250. / 255., 200. / 255.);
-    pub const COLOR15: Color = Color::from_rgb(128. / 255., 0., 0.);
-    pub const COLOR16: Color = Color::from_rgb(170. / 255., 255. / 255., 195. / 255.);
-    pub const COLOR17: Color = Color::from_rgb(128. / 255., 128. / 255., 0.);
-    pub const COLOR18: Color = Color::from_rgb(255. / 255., 216. / 255., 177. / 255.);
-    pub const COLOR19: Color = Color::from_rgb(0., 0., 117. / 255.);
-    pub const COLOR20: Color = Color::from_rgb(128. / 255., 128. / 255., 128. / 255.);
-    pub const COLOR21: Color = Color::from_rgb(255. / 255., 255. / 255., 255. / 255.);
-    pub const COLOR22: Color = Color::from_rgb(0., 0., 0.);
-
-}
-
-use colors::*;
+use common::colors::*;
 
 fn fr(fraction: f32) -> TrackSizingFunction {
     minmax(points(0.0), flex(fraction))
 }
-
 
 pub fn main() -> iced::Result {
     Example::run(Settings::default())
